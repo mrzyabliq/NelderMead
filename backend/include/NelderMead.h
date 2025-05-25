5ca3b565cd3abd66145d13da6699101adc85e787
данил саутенков <d.sautenkov@mail.ru>
2025-05-25 15:24:51 +0300

    Symplex class

--- backend/include/NelderMead.h
+++ backend/include/NelderMead.h
@@ -5,7 +5,7 @@
 #include <vector>
 
 #include "Parser.h"
-
+#include "Symplex.h"
 #ifdef _WIN32
 #ifdef NELDERMEAD_EXPORTS
 #define NELDERMEAD_API __declspec(dllexport)
@@ -16,60 +16,33 @@
 #define NELDERMEAD_API __attribute__((visibility("default")))
 #endif
 
-struct X {
-  std::vector<double> coordinates;
-  double value;
-
-  bool operator<(const X& other) const;
-  X operator+(const X& other) const;
-  X operator-(const X& other) const;
-  X operator*(int num) const;
-  X operator*(double num) const;
-  X operator/(double num) const;
-};
-
 class NELDERMEAD_API NelderMead {
-  std::vector<X> symplex;
-  Parser function;
-  int dims;
+  Symplex symplex;
+  std::string expression;
 
  public:
   NelderMead(std::string expression);
-  X Solver(double tolerance, double alpha, double beta, double gamma, double sigma);
-  X Solver(std::vector<double> init_point, double tolerance, double alpha, double beta, double gamma, double sigma);
-  X solverProcessing(double tolerance, double alpha, double beta, double gamma, double sigma);
+  X Solver(double init_tolerance, double alpha, double beta, double gamma,
+           double sigma);
+  X Solver(std::vector<double> init_point, double init_tolerance, double alpha,
+           double beta, double gamma, double sigma);
+
   int getDims();
   int getIterations();
-  std::vector<X> pointsForGraph();
   std::vector<double> getHistory();
 
  private:
-  void startPoint();
-  void startPoint(std::vector<double> init_point);
-  void Sort();
-  void reduction();
-  void removeBad(X point);
+  X solverProcessing(double tolerance);
+  bool condition(double tolerance);
   void chooseBest();
   void savePointsToLog(int iteration_num);
+
   void startLogFile();
   void finishLog();
   void saveHistory();
   void clearHistory();
-  void initKoefs(double alpha, double beta, double gamma, double sigma);
-  X computeCentroid();
-  X reflection(X centroid, X worst_point);
-  X expansion(const X& centroid, const X& reflected);
-  X contraction(const X& centroid, const X& worst);
-  double calcFunc(X x);
-  std::unordered_map<std::string, double> vectorToMap(
-      std::vector<double> coords);
   std::ofstream outfile;
-  double tolerance;
   std::vector<double> history;
-  double alpha;
-  double beta;
-  double gamma;
-  double sigma;
 };
 
 #ifdef __cplusplus
@@ -84,13 +57,15 @@ NELDERMEAD_API void SolveWithValue(NelderMeadHandle* handle, double* output,
                                    double* value);
 NELDERMEAD_API void SolveInit(NelderMeadHandle* handle, double* coordinates,
                               double* output);
-NELDERMEAD_API void SolveWithKoefs(NelderMeadHandle* handle, double tolerance, double alpha, double beta,
-                    double gamma, double sigma, double* output, double* value);
+NELDERMEAD_API void SolveWithKoefs(NelderMeadHandle* handle, double tolerance,
+                                   double alpha, double beta, double gamma,
+                                   double sigma, double* output, double* value);
 NELDERMEAD_API void SolveFull(NelderMeadHandle* handle, double* coordinates,
                               double* output, double* value);
-NELDERMEAD_API void SolveFullKoefs(NelderMeadHandle* handle, double* coordinates, double tolerance, double alpha,
-                    double beta, double gamma, double sigma, double* output,
-                    double* value);
+NELDERMEAD_API void SolveFullKoefs(NelderMeadHandle* handle,
+                                   double* coordinates, double tolerance,
+                                   double alpha, double beta, double gamma,
+                                   double sigma, double* output, double* value);
 NELDERMEAD_API void DestroyNelderMead(NelderMeadHandle* handle);
 NELDERMEAD_API void GetPointsForGraph(NelderMeadHandle* handle, double* output,
                                       int maxSize);
